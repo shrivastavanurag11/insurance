68f1a00d43423508a3f7dc5b129d390a4dfe4560
Anurag <Anurag@DESKTOP-NAO4C5L>
2024-08-29 22:02:27 +0530

    Jwttoken

---
+++ Models/DataTransferModel.cs
@@ -0,0 +1,9 @@
+﻿namespace insurance.Models
+{
+    public class DataTransferModel
+    {
+        public object? Data { get; set; }   //token model will be stored here
+        public string? Message { get; set; }
+        public bool  Success { get; set; }
+    }
+}
