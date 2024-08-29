e2230f29cbaba9610c362ecbb6abf13aac6abfb7
Anurag <Anurag@DESKTOP-NAO4C5L>
2024-08-29 23:46:11 +0530

    commitedcustomer

---
+++ Services/CustomerService.cs
@@ -0,0 +1,24 @@
+﻿namespace insurance.Services
+{
+    public interface ICustomerService
+    { 
+        
+    }
+
+    public class CustomerService:ICustomerService
+    {
+        public readonly IConfiguration config;
+
+        public CustomerService(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+        //display policy details
+        //search policy
+        //update customer details(phone , email , address)
+        public 
+
+        //
+    }
+}
