1426161cdba3bd382c62d3b46211901b1a7d5016
shatakshi171102 <shatakshi.1711@gmail.com>
2024-09-03 10:24:47 +0530

    Modified controllers and services

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,65 +1,318 @@
+using insurance.Models.Dbase;
+using insurance.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using insurance.Models.Dbase;
+using insurance.Services;
 
-namespace insurance.Controllers{
-  //[Route("api/[controller]")]
-  [ApiController]
 
-  public class CustomersController : ControllerBase
-  {
-      private readonly ApplicationDbContext _context;
-  
-      public CustomersController(ApplicationDbContext context)
-      {
-          _context = context;
-      }
+namespace insurance.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerService _service;
 
-      [HttpPost]
-      public IActionResult AddCustomer([FromBody] Customer customer)
-      {
-          _context.Customers.Add(customer);
-          _context.SaveChanges();
-  
-          return CreatedAtAction(nameof(GetCustomer), new { id = customer.CustomerId }, customer);
-      }
+        public CustomerController(ICustomerService service)
+        {
+            _service = service;
+        }
 
-      [HttpGet("{id}")]
-      public IActionResult GetCustomer(int id)
-      {
-          var customer = _context.Customers.Find(id);
-          if (customer == null)
-          {
-              return NotFound();
-          }
-  
-          return Ok(customer);
-      }
+        [HttpPost]
+        [Route("add")]
+        public async Task<IActionResult> AddCustomers([FromBody] Customer customer)
+        {
+            int result = await _service.AddCustomers(customer);
+            return Ok(result);
+        }
 
-      [HttpPut("{id}")]
-      public IActionResult UpdateCustomer(int id, [FromBody] Customer customer)
-      {
-          if (id != customer.CustomerId)
-          {
-              return BadRequest();
-          }
+        [HttpGet]
+        [Route("customer/{id}")]
+        public IActionResult GetCustomers(int id)
+        {
+            var customer = _service.GetCustomers(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
 
-          var existingCustomer = _context.Customers.Find(id);
-          if (existingCustomer == null)
-          {
-              return NotFound();
-          }
+            return Ok(customer);
+        }
 
-          existingCustomer.Name = customer.Name;
-          existingCustomer.Phone = customer.Phone;
-          existingCustomer.Email = customer.Email;
-          existingCustomer.Address = customer.Address;
-  
-          _context.SaveChanges();
-  
-          return NoContent();
-      }
-  }
+        [HttpPut]
+        [Route("update/{id}")]
+        public async Task<IActionResult> UpdateCustomers(int id, [FromBody] Customer customer)
+        {
+            if (id != customer.CustomerId)
+            {
+                return BadRequest();
+            }
 
+            bool result = await _service.UpdateCustomers(customer);
+            if (result)
+            {
+                return NoContent();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
+//using PolicyManagementAPI.Models.Dbase;
+//using PolicyManagementAPI.Services;
+
+//namespace PolicyManagementAPI.Controllers
+//{
+//    //[Route("api/[controller]")]
+//    [ApiController]
+//    public class CustomerController : Controller
+//    {
+//        private readonly ICustomerService service;
+//        public CustomerController(ICustomerService service)
+//        {
+//            this.service = service;
+//        }
+
+//        [HttpGet]
+//        [Route("customer")]
+//        public IActionResult AllCustomers()  //action method
+//        {
+//            List<Customer> customers = service.GetCustomer();
+//            return Ok(customers);
+//        }
+
+//        [HttpGet]
+//        [Route("customer/{CustomerId}")]
+//        public IActionResult GetCustomers(int CustomerId)
+//        {
+//            Customer? c = service.GetCustomers(CustomerId);
+//            if (c == null)
+//                return NotFound();
+//            else
+//                return Ok(c);
+//        }
+
+//        [HttpPost]
+//        [Route("add")]
+//        public async Task<IActionResult> AddCustomers(Customer c)
+//        {
+//            int result = await service.AddCustomers(c);
+//            return Ok(result);
+//        }
+//        //[HttpPut]
+//        //[Route("update")]
+//        //public async Task<IActionResult> UpdateCustomers(Customer c)
+//        //{
+//        //    bool result = await service.UpdateCustomers(c);
+//        //    if (result)
+//        //        return Ok();
+//        //    else
+//        //        return NotFound();
+//        //}
+
+//        [HttpPut]
+//        [Route("update/{id}")]
+//        public IActionResult UpdateCustomers(int id, [FromBody] Customer customer)
+//        {
+//            if (id != customer.CustomerId)
+//            {
+//                return BadRequest();
+//            }
+
+//            var existingCustomer = service.Customers.Find(id);
+//            if (existingCustomer == null)
+//            {
+//                return NotFound();
+//            }
+
+//            existingCustomer.Name = customer.Name;
+//            existingCustomer.Phone = customer.Phone;
+//            existingCustomer.Email = customer.Email;
+//            existingCustomer.Address = customer.Address;
+
+//            _context.SaveChanges();
+
+//            return NoContent();
+//        }
+//        [HttpDelete]
+//        [Route("delete/{EmployeeId}")]
+//        public IActionResult DeleteCustomers(int EmployeeId)
+//        {
+//            bool result = service.DeleteCustomers(EmployeeId);
+//            if (result)
+//            {
+//                return Content("Record Deleted..");
+//            }
+//            else
+//                return NotFound();
+
+//        }
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+////using Microsoft.AspNetCore.Http;
+////using Microsoft.AspNetCore.Mvc;
+////using PolicyManagementAPI.Models.Dbase;
+////using PolicyManagementAPI.Services;
+
+////namespace insurance.Controllers
+////{
+////    //[Route("api/[controller]")]
+////    [ApiController]
+
+////    public class CustomerController : ControllerBase
+////    {
+////        private readonly ApplicationDbContext _context;
+
+////        public CustomerController(ApplicationDbContext context)
+////        {
+////            _context = context;
+////        }
+
+////        [HttpPost]
+////        public IActionResult AddCustomers([FromBody] Customer customer)
+////        {
+////            _context.Customers.Add(customer);
+////            _context.SaveChanges();
+
+////            return CreatedAtAction(nameof(GetCustomers), new { id = customer.CustomerId }, customer);
+////        }
+
+////        [HttpGet("{id}")]
+////        public IActionResult GetCustomers(int id)
+////        {
+////            var customer = _context.Customers.Find(id);
+////            if (customer == null)
+////            {
+////                return NotFound();
+////            }
+
+////            return Ok(customer);
+////        }
+
+////        [HttpPut("{id}")]
+////        public IActionResult UpdateCustomers(int id, [FromBody] Customer customer)
+////        {
+////            if (id != customer.CustomerId)
+////            {
+////                return BadRequest();
+////            }
+
+////            var existingCustomer = _context.Customers.Find(id);
+////            if (existingCustomer == null)
+////            {
+////                return NotFound();
+////            }
+
+////            existingCustomer.Name = customer.Name;
+////            existingCustomer.Phone = customer.Phone;
+////            existingCustomer.Email = customer.Email;
+////            existingCustomer.Address = customer.Address;
+
+////            _context.SaveChanges();
+
+////            return NoContent();
+////        }
+////    }
+
+////}
+
+
+////using Microsoft.AspNetCore.Http;
+////using Microsoft.AspNetCore.Mvc;
+////using PolicyManagementAPI.Models.Dbase;
+////using PolicyManagementAPI.Services;
+
+////namespace insurance.Controllers
+////{
+////    //[Route("api/[controller]")]
+////    [ApiController]
+////    public class CustomerController : ControllerBase
+////    {
+////        private readonly ApplicationDbContext _context;
+
+////        public CustomerController(ApplicationDbContext context)
+////        {
+////            _context = context;
+////        }
+
+////        [HttpPost]
+////        [Route("add")]
+////        public IActionResult AddCustomers([FromBody] Customer customer)
+////        {
+////            _context.Customers.Add(customer);
+////            _context.SaveChanges();
+
+////            return CreatedAtAction(nameof(GetCustomers), new { id = customer.CustomerId }, customer);
+////        }
+
+////        [HttpGet]
+////        [Route("customer/{id}")]
+////        public IActionResult GetCustomers(int id)
+////        {
+////            var customer = _context.Customers.Find(id);
+////            if (customer == null)
+////            {
+////                return NotFound();
+////            }
+
+////            return Ok(customer);
+////        }
+
+////        [HttpPut]
+////        [Route("update/{id}")]
+////        public IActionResult UpdateCustomers(int id, [FromBody] Customer customer)
+////        {
+////            if (id != customer.CustomerId)
+////            {
+////                return BadRequest();
+////            }
+
+////            var existingCustomer = _context.Customers.Find(id);
+////            if (existingCustomer == null)
+////            {
+////                return NotFound();
+////            }
+
+////            existingCustomer.Name = customer.Name;
+////            existingCustomer.Phone = customer.Phone;
+////            existingCustomer.Email = customer.Email;
+////            existingCustomer.Address = customer.Address;
+
+////            _context.SaveChanges();
+
+////            return NoContent();
+////        }
+////    }
+////}
 
--- Services/CustomerService.cs
+++ Services/CustomerService.cs
@@ -1,61 +1,121 @@
-using Microsoft.Extensions.Configuration;
-using System.Linq;
+using System.ComponentModel;
+using insurance.Models.Dbase;
+using insurance.Services;
 
 namespace insurance.Services
 {
     public interface ICustomerService
     {
-        Customer GetCustomer(int id);
-        void AddCustomer(Customer customer);
-        void UpdateCustomer(int id, Customer customer);
-        // Add methods for displaying policy details and searching policy
-    }
+        List<Customer> GetAllCustomers();
+        Customer? GetCustomers(int CustomerId);
+        bool DeleteCustomers(int CustomerId);
+        Task<int> AddCustomers(Customer c);
+        Task<bool> UpdateCustomers(Customer c);
 
+    }
     public class CustomerService : ICustomerService
     {
-        private readonly ApplicationDbContext _context;
-        public readonly IConfiguration config;
 
-        public CustomerService(ApplicationDbContext context, IConfiguration config)
+        private readonly ApplicationDbContext context;
+        public CustomerService(ApplicationDbContext context)
         {
-            _context = context;
-            this.config = config;
+            this.context = context;
         }
 
-        public Customer GetCustomer(int id)
+
+        public List<Customer> GetAllCustomers()
         {
-            return _context.Customers.Find(id);
+            var list = context.Policy.ToList();
+            return list;
+        }
+        public Customer? GetCustomers(int CustomerId)
+        {
+            var customer = context.Policy.SingleOrDefault(c => c.CustomerId == CustomerId);
+            return customer;
+        }
+        //public bool DeleteCustomer(int CustomerId)
+        //{
+        //    var c = (from a in context.Customers where a.CustomerId == CustomerId select a).FirstOrDefault();
+        //    if (c == null)
+        //        return false;
+        //    else
+        //    {
+        //        context.Customers.Remove(c);
+        //        context.SaveChanges();
+        //        return true;
+        //    }
+        //    //throw new NotImplementedException();
+        //}
+        public async Task<int> AddCustomers(Customer c)
+        {
+            context.Policy.Add(c);
+            var i = await context.SaveChangesAsync();
+            return i;
         }
 
-        public void AddCustomer(Customer customer)
+        public Customer? GetCustomer(int CustomerId)
         {
-            _context.Customers.Add(customer);
-            _context.SaveChanges();
+            throw new NotImplementedException();
         }
 
-        public void UpdateCustomer(int id, Customer customer)
+
+
+        public async Task<bool> UpdateCustomers(Customer c)
         {
-            var existingCustomer = _context.Customers.Find(id);
-            if (existingCustomer != null)
+            Customer? customer = context.Policy.SingleOrDefault(a => a.CustomerId == c.CustomerId);
+            if (customer == null) return false;
+            else
             {
-                existingCustomer.Name = customer.Name;
-                existingCustomer.Phone = customer.Phone;
-                existingCustomer.Email = customer.Email;
-                existingCustomer.Address = customer.Address;
-                _context.SaveChanges();
+                customer.Name = customer.Name;
+                customer.Phone = customer.Phone;
+                customer.Email = customer.Email;
+                customer.Address = customer.Address;
+
+                await context.SaveChangesAsync();
+                return true;
             }
         }
 
-        // Method to display policy details
-        public Policy GetPolicyDetails(int policyId)
+        public bool DeleteCustomers(int CustomerId)
         {
-            return _context.Policies.Find(policyId);
+            var c = (from a in context.Policy where a.CustomerId == CustomerId select a).FirstOrDefault();
+            if (c == null)
+                return false;
+            else
+            {
+                context.Policy.Remove(c);
+                context.SaveChanges();
+                return true;
+            }
         }
 
-        // Method to search policy
-        public IQueryable<Policy> SearchPolicy(string policyName)
-        {
-            return _context.Policies.Where(p => p.Name.Contains(policyName));
-        }
+        //public async Task<bool> UpdateCustomers(Customer c)
+        //{
+        //    Customer? customers = context.Customers.SingleOrDefault(c => c.Code == em.Code);
+        //    if (emp == null) return false;
+        //    else
+        //    {
+        //        emp.Name = em.Name;
+        //        emp.Salary = em.Salary;
+
+        //        await context.SaveChangesAsync();
+        //        return true;
+        //    }
+        //}
+        /*  public bool UpdateEmp2(Emp em)
+            {
+                Emp? emp = context.Emps.SingleOrDefault(c => c.Code == em.Code);
+                if (emp == null) return false;
+                else
+                {
+                    emp.Name = em.Name;
+                    emp.Salary = em.Salary;
+
+                    context.SaveChanges();
+                    return true;
+                }
+            }
+        */
     }
 }
+
